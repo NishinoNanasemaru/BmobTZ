d9d1933b86819bb759f3aec9d7bcbc5337ef1a20
NishinoNanasemaru <932064280@qq.com>
2016-11-14 18:05:44 +0800

    20161114 TZ

--- app/src/main/java/com/tz/bmobtz/MainActivity.java
+++ app/src/main/java/com/tz/bmobtz/MainActivity.java
@@ -10,9 +10,13 @@ import android.widget.Toast;
 
 import com.tz.bmobtz.been.FeedBack;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import cn.bmob.push.BmobPush;
 import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobInstallation;
+import cn.bmob.v3.BmobPushManager;
 import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.FindListener;
@@ -22,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     EditText memberName, memberNum;
     TextView content;
-    Button send, demand, obtain;
+    Button send, demand, obtain, push;
     FeedBack mFeedBack;
 
     @Override
@@ -30,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Bmob.initialize(this, "411c42edaac6a8926d3d635362090f22");
+        BmobInstallation.getCurrentInstallation().save();
+        BmobPush.startWork(this);
 
         initView();
 
@@ -42,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         send = (Button) findViewById(R.id.bt_send);
         demand = (Button) findViewById(R.id.bt_demand);
         obtain = (Button) findViewById(R.id.bt_obtain);
+        push = (Button) findViewById(R.id.bt_push);
     }
 
     public void doClick(View view) {
@@ -50,25 +57,66 @@ public class MainActivity extends AppCompatActivity {
                 mFeedBack = new FeedBack();
                 String name = memberName.getText().toString();
                 String num = memberNum.getText().toString();
-                if (name.equals(null) || num.equals(null)) {
+                if (name.equals("") || num.equals("")) {
+                    Toast.makeText(this, "输入框不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    mFeedBack.setMemberName(name);
+                    mFeedBack.setMemberNum(num);
+                    mFeedBack.save(new SaveListener<String>() {
+                        @Override
+                        public void done(String s, BmobException e) {
+                            if (e == null) {
+                                memberName.setText("");
+                                memberNum.setText("");
+                                Toast.makeText(MainActivity.this, "添加数据成功", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(MainActivity.this, "添加数据失败", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+                break;
+            case R.id.bt_demand:
+                String named = memberName.getText().toString();
+                String numd = memberNum.getText().toString();
+                if (named.equals("") & numd.equals("")) {
+                    Toast.makeText(this, "输入框不能全为空", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                mFeedBack.setMemberName(name);
-                mFeedBack.setMemberNum(num);
-                mFeedBack.save(new SaveListener<String>() {
+                BmobQuery<FeedBack> q1 = new BmobQuery<FeedBack>();
+                BmobQuery<FeedBack> q2 = new BmobQuery<FeedBack>();
+                BmobQuery<FeedBack> query1;
+                List<BmobQuery<FeedBack>> andQuery = new ArrayList<BmobQuery<FeedBack>>();
+                if (!named.equals("") & !numd.equals("")) {
+                    query1 = new BmobQuery<FeedBack>();
+                    q1.addWhereEqualTo("memberName", named);
+                    q2.addWhereEqualTo("memberNum", numd);
+                    andQuery.add(q1);
+                    andQuery.add(q2);
+                    query1.and(andQuery);
+                } else if (numd.equals("")) {
+                    q2.addWhereEqualTo("memberName", named);
+                    query1 = q2;
+                } else {
+                    q1.addWhereEqualTo("memberNum", numd);
+                    query1 = q1;
+                }
+                query1.findObjects(new FindListener<FeedBack>() {
                     @Override
-                    public void done(String s, BmobException e) {
+                    public void done(List<FeedBack> list, BmobException e) {
+                        String string = "";
                         if (e == null) {
-                            memberName.setText("");
-                            memberNum.setText("");
-                            Toast.makeText(MainActivity.this, "添加数据成功", Toast.LENGTH_SHORT).show();
+                            for (FeedBack feedBack : list) {
+                                string += feedBack.getMemberName() + ":"
+                                        + feedBack.getMemberNum() + "\n";
+                            }
+                            content.setText(string);
                         } else {
-                            Toast.makeText(MainActivity.this, "添加数据失败", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, "失败", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
-                break;
-            case R.id.bt_demand:
                 break;
             case R.id.bt_obtain:
                 content.setText("");
@@ -78,7 +126,6 @@ public class MainActivity extends AppCompatActivity {
                     public void done(List<FeedBack> list, BmobException e) {
                         String string = "";
                         if (e == null) {
-
                             for (FeedBack feedBack : list) {
                                 string += feedBack.getMemberName() + ":"
                                         + feedBack.getMemberNum() + "\n";
@@ -87,8 +134,11 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
-
-
+                break;
+            case R.id.bt_push:
+                BmobPushManager push = new BmobPushManager();
+                push.pushMessageAll("test");
+                //Toast.makeText(this,"0",Toast.LENGTH_SHORT).show();
                 break;
             default:
                 break;
